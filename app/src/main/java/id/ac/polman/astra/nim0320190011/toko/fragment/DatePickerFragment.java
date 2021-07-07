f43d1ba020b714982d3e31b35cbc3a61a59ee634
TeddyantoIJ <teddyanto.ij@gmail.com>
2021-07-07 10:52:24 +0700

    fotofoto

---
+++ app/src/main/java/id/ac/polman/astra/nim0320190011/toko/fragment/DatePickerFragment.java
@@ -0,0 +1,65 @@
+package id.ac.polman.astra.nim0320190011.toko.fragment;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DatePickerFragment extends DialogFragment {
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    private Callbacks callbacks;
+
+    interface Callbacks{
+        public void onDateSelected(Date date);
+    }
+
+    public static DatePickerFragment newInstance(Date date){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE,date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        DatePickerDialog.OnDateSetListener dateListener;
+
+        dateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int month, int dayOfMonth) {
+                Date resultDate = new GregorianCalendar(year,month,dayOfMonth)
+                        .getTime();
+                callbacks = (Callbacks) getTargetFragment();
+                callbacks.onDateSelected(resultDate);
+            }
+        };
+
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int initialYear = calendar.get(Calendar.YEAR);
+        int initialMonth = calendar.get(Calendar.MONTH);
+        int initialDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(
+                requireContext(),
+                dateListener,
+                initialYear,
+                initialMonth,
+                initialDay
+        );
+    }
+}
--- app/src/main/java/id/ac/polman/astra/nim0320190011/toko/fragment/Fragment_daftar_toko.java
+++ app/src/main/java/id/ac/polman/astra/nim0320190011/toko/fragment/Fragment_daftar_toko.java
@@ -1,7 +1,14 @@
 package id.ac.polman.astra.nim0320190011.toko.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +20,30 @@ import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import id.ac.polman.astra.nim0320190011.toko.R;
+import id.ac.polman.astra.nim0320190011.toko.Utils.PictureUtils;
 
-public class Fragment_daftar_toko extends Fragment {
+import static androidx.core.view.ViewCompat.jumpDrawablesToCurrentState;
+
+public class Fragment_daftar_toko extends Fragment
+    implements DatePickerFragment.Callbacks{
     private static final String TAG = "Fragment_daftar_toko";
+
+    private static final String DIALOG_DATE = "DialogDate";
+
+    private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_PHOTO_DIRI = 1;
+    private static final int REQUEST_PHOTO_KTP = 2;
+    private static final int REQUEST_PHOTO_TOKO = 3;
 
     private EditText mNama;
     private EditText mEmail;
@@ -51,6 +74,18 @@ public class Fragment_daftar_toko extends Fragment {
     private EditText mPassword;
     private EditText mPasswordVer;
 
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Override
+    public void onDateSelected(Date date) {
+        String newDate = formatter.format(date);
+
+        updateUI(newDate);
+    }
+
+    private void updateUI(String newDate){
+        mTanggalLahir.setText(newDate);
+    }
 
     public static Fragment_daftar_toko newInstance(){
         return new Fragment_daftar_toko();
@@ -61,6 +96,183 @@ public class Fragment_daftar_toko extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pendaftaran_toko, container, false);
 
+        mNama = (EditText) view.findViewById(R.id.nama_pemilik);
+        mEmail = (EditText) view.findViewById(R.id.email);
+
+        mJenisKelamin = (RadioGroup) view.findViewById(R.id.jenis_kelamin);
+        int selectedId = mJenisKelamin.getCheckedRadioButtonId();
+        mJenisKelaminSelected = (RadioButton) view.findViewById(selectedId);
+
+        mTempatLahir = (EditText) view.findViewById(R.id.tempat_lahir);
+
+        mTanggalLahir = (EditText) view.findViewById(R.id.tanggal_lahir);
+        mTanggalLahir.setText(formatter.format(new Date()));
+        mTanggalLahir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getParentFragmentManager();
+                //DatePickerFragment dialog = new DatePickerFragment();
+                DatePickerFragment dialog =
+                        DatePickerFragment.newInstance(new Date());
+                dialog.setTargetFragment(Fragment_daftar_toko.this,REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
+
+        mAlamat = (EditText) view.findViewById(R.id.alamat);
+        mAlamatToko = (EditText) view.findViewById(R.id.alamat_toko);
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        mFotoDiriButton = (ImageButton) view.findViewById(R.id.open_came_1);
+        mFotoKTPButton = (ImageButton) view.findViewById(R.id.open_came_2);
+        mFotoTokoButton =  (ImageButton) view.findViewById(R.id.open_came_3);
+
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(packageManager.resolveActivity(captureImage,
+                PackageManager.MATCH_DEFAULT_ONLY) == null){
+            mFotoDiriButton.setEnabled(false);
+            mFotoKTPButton.setEnabled(false);
+            mFotoTokoButton.setEnabled(false);
+        }
+
+        mFotoDiriButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, mFotoDiriUri);
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+                for(ResolveInfo activity : cameraActivities){
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,
+                            mFotoDiriUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+                startActivityForResult(captureImage, REQUEST_PHOTO_DIRI);
+            }
+        });
+        mFotoKTPButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, mFotoKTPUri);
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+                for(ResolveInfo activity : cameraActivities){
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,
+                            mFotoKTPUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+                startActivityForResult(captureImage, REQUEST_PHOTO_KTP);
+            }
+        });
+        mFotoTokoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, mFotoTokoUri);
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+                for(ResolveInfo activity : cameraActivities){
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,
+                            mFotoTokoUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+                startActivityForResult(captureImage, REQUEST_PHOTO_TOKO);
+            }
+        });
+
+        mFotoDiriView = (ImageView) view.findViewById(R.id.foto_diri);
+        mFotoKTPView = (ImageView) view.findViewById(R.id.foto_ktp);
+        mFotoTokoView = (ImageView) view.findViewById(R.id.foto_toko);
+
+
+        mUsername = (EditText) view.findViewById(R.id.username);
+        mPassword = (EditText) view.findViewById(R.id.password);
+        mPasswordVer = (EditText) view.findViewById(R.id.password_verif);
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mFotoDiriFile = new File(getContext().getApplicationContext().getFilesDir(), "foto_diri");
+        mFotoDiriUri = FileProvider.getUriForFile(requireActivity(),
+                "id.ac.polman.astra.nim0320190011.toko.fileprovider",
+                mFotoDiriFile
+        );
+
+        mFotoKTPFile = new File(getContext().getApplicationContext().getFilesDir(), "foto_ktp");
+        mFotoKTPUri = FileProvider.getUriForFile(requireActivity(),
+                "id.ac.polman.astra.nim0320190011.toko.fileprovider",
+                mFotoKTPFile
+        );
+
+        mFotoTokoFile = new File(getContext().getApplicationContext().getFilesDir(), "foto_toko");
+        mFotoTokoUri = FileProvider.getUriForFile(requireActivity(),
+                "id.ac.polman.astra.nim0320190011.toko.fileprovider",
+                mFotoTokoFile
+        );
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_PHOTO_DIRI){
+            requireActivity().revokeUriPermission(mFotoDiriUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
+        }
+        if(requestCode == REQUEST_PHOTO_KTP){
+            requireActivity().revokeUriPermission(mFotoKTPUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
+        }
+        if(requestCode == REQUEST_PHOTO_TOKO){
+            requireActivity().revokeUriPermission(mFotoTokoUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            updatePhotoView();
+        }
+    }
+
+    private void updatePhotoView(){
+        if(mFotoDiriFile == null || !mFotoDiriFile.exists()){
+            mFotoDiriView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap = PictureUtils.getScaleBitmap(
+                    mFotoDiriFile.getPath(), requireActivity()
+            );
+            mFotoDiriView.setImageBitmap(bitmap);
+        }
+
+        if(mFotoKTPFile == null || !mFotoKTPFile.exists()){
+            mFotoKTPView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap = PictureUtils.getScaleBitmap(
+                    mFotoKTPFile.getPath(), requireActivity()
+            );
+            mFotoKTPView.setImageBitmap(bitmap);
+        }
+
+        if(mFotoTokoFile == null || !mFotoTokoFile.exists()){
+            mFotoTokoView.setImageDrawable(null);
+        }else{
+            Bitmap bitmap = PictureUtils.getScaleBitmap(
+                    mFotoTokoFile.getPath(), requireActivity()
+            );
+            mFotoTokoView.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        try{
+            requireActivity().revokeUriPermission(mFotoDiriUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }catch (Exception e){
+            Log.e(TAG, "onDetach: ", e);
+        }
+
     }
 }
